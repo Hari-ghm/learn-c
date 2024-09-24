c9de27aeaa4306ea8cee283c3b3348af49c08180
Hari-ghm <hmadhav2006@gmail.com>
2024-09-24 11:33:01 +0530

    created function_pointers.c

---
+++ funtion_pointers.c
@@ -0,0 +1,13 @@
+// SIMPLE FUNCTION
+
+#include<stdio.h>
+void message();
+int main(){
+    message();
+    printf("this is line 1./n");
+    return 0;
+}
+
+void message(){
+    printf("this line is inside function message./n");
+}
